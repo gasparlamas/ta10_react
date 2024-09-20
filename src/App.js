5622e4b5f0550f24dd06727bf7c3ab2435c8ada7
gasparlamas <gasparlamas32@gmail.com>
2024-09-20 17:18:16 -0300

    ta10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-function App() {
+const getUsers = async () => {
+  const usersFetch = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await usersFetch.json();
+  return users;
+};
+
+const ShowUsers = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((users) => setUsers(users));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <div>{user.email}</div>
+            {user.username}
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <ShowUsers />
+    </div>
+  );
+};
 
 export default App;
